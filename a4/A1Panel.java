641176cf90b0a5ce7a8b7f845b49f8d4ab4df956
C-A de Salaberry <cadesalaberry@yahoo.com>
2012-11-28 21:04:49 -0500

    Initial code.

---
+++ a4/A1Panel.java
@@ -0,0 +1,12 @@
+package a4;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class A1Panel extends JPanel implements ActionListener{
+	
+	public void actionPerformed(ActionEvent e) {
+	}
+}
